79ad09ab78b4c3ef2e717a7d9df38266bd384f0f
anjulsingh999 <anjulsingh462@gmail.com>
2024-09-03 10:25:19 +0530

    ok

--- src/Pages/Market_place.jsx
+++ src/Pages/Market_place.jsx
@@ -1,15 +1,11 @@
 import React from 'react'
-import Market_Place_Card from '../components/Market Place/Market_Place_Card'
-import Market_place_navbar from '../components/Market Place/Market_place_navbar'
 import Market_Place_navigation from '../components/Market Place/Market_Place_navigation'
 
 const Market_place = () => {
   return (
     <>
         <div>
-            {/* <Market_place_navbar/> */}
             <Market_Place_navigation/>
-           {/* <div className='py-9'> <Market_Place_Card/></div> */}
         </div>
     </>
   )
