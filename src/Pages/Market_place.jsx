694694ff4e8aa1c38806d9f1a60f3fe4b4bc116d
anjulsingh999 <anjulsingh462@gmail.com>
2024-09-07 00:07:23 +0530

    'ok'

--- src/Pages/Market_place.jsx
+++ src/Pages/Market_place.jsx
@@ -5,16 +5,7 @@ import {farmer} from '../assets/image'
 const Market_place = () => {
   return (
     <>
-        <div className='w-full h-screen bg-black text-red-500 flex justify-center items-center flex-row gap-9'>
-            {/* <Market_Place_navigation/> */}
-            <div className='h-[300px] w-[300px] bg-red-500 rounded-[15px] flex justify-center items-center flex-col gap-5'>
-              <img src={farmer} className='h-[150px] w-[150px]'/>
-              <div className='text-[24px] text-white font-bold'>As a Farmer</div>
-            </div>
-            <div className='h-[300px] w-[300px] bg-red-500 rounded-[15px]'></div>
-            
-
-        </div>
+      <Market_Place_navigation/>
     </>
   )
 }
